fbcfa28ab95930e0deca5cfa26d3d951f1b9691f
FlutterRaj <rajkar921@gmail.com>
2021-02-11 15:09:12 +0530

    add .env variables and change sqlite to postgres

--- KEYS.py
+++ KEYS.py
@@ -1,4 +1,4 @@
 import os
 
-email = os.environ.get('MY_EMAIl')
-password = os.environ.get('MY_PASSWORD')
+email = os.environ.get('MY_EMAIL')
+password = os.environ.get('MY_EMAIL_PASSWORD')
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import smtplib
 from datetime import date
 from functools import wraps
+import os
 
 from flask import Flask, render_template, redirect, url_for, flash, request, abort
 from flask_bootstrap import Bootstrap
@@ -15,12 +16,12 @@ from KEYS import email, password
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm, ContactForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
